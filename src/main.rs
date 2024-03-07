067400745933446cdf8ab382c7d2c913f09a8695
Tarek Ali <tarekali@Tareks-MacBook-Air.local>
2024-03-07 17:43:33 -0600

    init request no parsing yet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-fn main() {
+use tokio::runtime::Runtime;
+use reqwest;
 
+#[tokio::main]
+async fn main() {
+ public_surplus_auction("3474529").await;
+}
+
+async fn public_surplus_auction(id: &str) -> Result<(), reqwest::Error> {
+    let url = format!("https://www.publicsurplus.com/sms/auction/view?auc={}", id);
+
+    let res = reqwest::get(&url).await?;
+
+    let body = res.text().await?;
+
+    println!("Body: \n{}", body);
+
+    Ok(())
 }
 
